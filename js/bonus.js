9c3ca7e72e4baa646155d732f61701f6d3c01c37
larryg727 <larrygonzales727@gmail.com>
2017-04-14 16:49:12 -0500

    -completed the dom query exercise

--- js/bonus.js
+++ js/bonus.js
@@ -28,27 +28,48 @@
     console.log(rest());
 
     //---------medium-------
+//     var count = 0
+//     function wordCount(string) {
+//         var holder = string.split(" ");
+//         holder.sort();
+//
+//         holder.forEach(function (word) {
+//             for(var i = 0; i > holder.length; i++){
+//                 if (holder.indexOf(word) >= 0){
+//                     holder.splice(this.indexOf(word), 1);
+//                     count++
+//                 }else {
+//                     return word + count;
+//                 }
+//             }
+//
+//
+//         });
+//     }
+//
+//         console.log(count);
+//     console.log(wordCount("I am testing"));
+// console.log(count);
 
-    function wordCount(string) {
-        var holder = string.split(" ");
-        holder.sort();
-
-        holder.forEach(function (word) {
-            var count = 0
-            for(var i = 0; i > holder.length; i++){
-                if (this.indexOf(word) >= 0){
-                    this.splice(this.indexOf(word), 1);
-                    count++
-                }
-
-                    return word + count;
-            }
-
-
-        });
+    function range(min, max, step){
+        var returnArray = [];
+        var span = max - min + 1;
+        for(var i = 0; i < span; i += step){
+            returnArray.push(min + i);
+        }
+        return returnArray;
     }
-console.log(holder);
-        console.log(count);
-    console.log(wordCount("I am testing"));
+console.log(range(10, 20, 2));
+
+
+    var numbers = [1, 2, 3, 4, 5, 6];
+    function chunk(array, span){
+        var piece = array[i]
+        for(var i = 0; i < array.length; i++)
+            var temp = array[i] + array[i+1];
+    }
+
+    chunk(numbers, 2);
+
 
 })();
