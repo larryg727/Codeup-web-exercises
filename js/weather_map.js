cc718815f642b8060b4f816d741b41d79b7271af
larryg727 <larrygonzales727@gmail.com>
2017-04-30 12:30:02 -0500

    -fixed day function and incoperated it into initial weather load. also incorporated today weather ajax request into the one main load.

--- js/weather_map.js
+++ js/weather_map.js
@@ -28,6 +28,14 @@ $(document).ready(function () {
             cnt: "10"
         }).done(function (data) {
             data.list.forEach(function (el, i) {
+                today = new Date().getDay();
+                if(i === 0){
+                    day = "Today";
+                }else if (today + i < 7) {
+                    day = daysOfWeek[today + i];
+                } else {
+                        day = daysOfWeek[today + i - 7];
+                }
                 var appendStr = '';
                 var appendStrLeft = " ";
                 var appendStrRight = ' ';
@@ -36,6 +44,7 @@ $(document).ready(function () {
                 var maxTemp = Math.round(data.list[i].temp.max);
                 var minTemp = Math.round(data.list[i].temp.min);
                 var iconUrl = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
+                appendStr +=("<h2>" + day + "</h2>");
                 appendStr += ("<h3>" + maxTemp + "&deg/" + minTemp + "&deg</h3>");
                 appendStr += ("<img src='" + iconUrl + "' alt='Icon'>");
                 appendStr += ("<p><strong>" + data.list[i].weather[0].main + ":</strong> " + data.list[i].weather[0].description + "</p>");
@@ -173,99 +182,7 @@ $(document).ready(function () {
 
     }
 
-    // //separate function for today tab. too many differences
-    // function loadTodayWeather() {
-    //     $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-    //         APPID: "a824ef2e2591bd239228beab33789010",
-    //         lat: lat,
-    //         lon: lng,
-    //         units: "imperial",
-    //         cnt: "1"
-    //     }).done(function (data) {
-    //         console.log(data);
-    //         data.list.forEach(function (el, i) {
-    //             var appendStrLeft = " ";
-    //             var appendStrRight = ' ';
-    //             var appendStrCenter = ' ';
-    //             var maxTemp = Math.round(data.list[i].temp.max);
-    //             var minTemp = Math.round(data.list[i].temp.min);
-    //             var iconUrl = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png";
-    //             appendStrLeft += ("<h3>High: " + maxTemp + "&deg</h3>");
-    //             appendStrLeft += ("<h3>Low: " + minTemp + "&deg</h3>");
-    //             appendStrRight += ("<h3><strong>" + data.list[i].weather[0].main + ":</strong> " + data.list[i].weather[0].description + "</h3>");
-    //             appendStrRight += ("<img src='" + iconUrl + "' alt='Icon'>");
-    //             appendStrCenter += ("<p><strong>Humidity: </strong>" + data.list[i].humidity + "</p>");
-    //             appendStrCenter += ("<p><strong>Wind: </strong>" + data.list[i].speed + "</p>");
-    //             appendStrCenter += ("<p><strong>Pressure: </strong>" + data.list[i].pressure + "</p>");
-    //
-    //             $("#todayLeft").html(appendStrLeft);  //inserting new weather
-    //             $('#todayCenter').html(appendStrCenter);
-    //             $("#todayRight").html(appendStrRight);
-    //
-    //             infoContent = "<img src='" + iconUrl + "' alt='Icon'>" + "<h3>Todays high: " + Math.round(data.list[0].temp.max) + "&deg</h3><h3>Today's low: " + Math.round(data.list[0].temp.min) + "&deg</h3>";
-    //             infowindow.setContent(infoContent);
-    //         });
-    //         $("#currentCity").html(data.city.name);  //update current city
-    //     });
-    // -----------running second ajax request JUST to receive current temp. ... kind of buggy.. not worth it.
-    // $.get("http://api.openweathermap.org/data/2.5/weather", {
-    //     APPID: "a824ef2e2591bd239228beab33789010",
-    //     lat: lat,
-    //     lon: lng,
-    //     units: "imperial"
-    // }).done(function (data) {
-    //     console.log(data);
-    //     var newStr = '';
-    //     var currentTemp = Math.round(data.main.temp);
-    //     newStr += ("<h3>Current Temp:  " + currentTemp + "&deg</h3>");
-    //     $(".todayBox").append(function(n, h){
-    //         $(".todayBox").html("");
-    //         return newStr + h;
-    //     });
-    //
-    // });
-// }
 
-// function runToday(){
-//     if($(".todayBox").css("display") !== "none"){
-//         loadTodayWeather();
-//     }else{
-//         loadWeather();
-//     }
-// }
-
-function dayFinder() {
-    today = new Date().getDay();
-    for (day = 1; day <= 4; day++) {
-        if (today + day < 7) {
-            $(".daysId3").children().first().next().text(daysOfWeek[today + 1]);
-            $(".daysId").children().first().next().next().text(daysOfWeek[today + 2]);
-        } else {
-            $(".daysId3").children().first().next().text(daysOfWeek[today + 1 - 7]);
-            $(".daysId3").children().first().next().next().text(daysOfWeek[today + 2 - 7]);
-        }
-    }
-}
-function dayFinderfive() {
-    today = new Date().getDay();
-    for (var i = 1; i < 5; i++) {
-        idVar = "#day" + i + ".fiveDayBox";
-        if (today + day < 7) {
-            day = daysOfWeek[today + i];
-            console.log(idVar);
-            $(idVar).append(function (n, h) {
-                return day + h;
-            });
-        } else {
-            $(idVar).append(function (n, h) {
-                day = daysOfWeek[today + i - 7];
-                console.log(idVar);
-                return day + h;
-            });
-        }
-    }
-}
-dayFinder();
 
 initializeMap();  // initial map load
 loadWeather(); // initial weather load
